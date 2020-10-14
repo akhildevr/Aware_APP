a29f7febb93604b14c6477d32df6d4eb0f95a855
akhildevr <47802364+akhildevr@users.noreply.github.com>
2020-10-14 17:37:51 +0530

    Update main.dart

--- aware_app/lib/main.dart
+++ aware_app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-//import './pages/login.dart';
+import './pages/login.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      // home: ,
+       home: loginPage(),
     );
   }
 }
